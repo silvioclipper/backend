a47ac52e74bab5ded997d3b741a60a8affaf40d9
silvioclipper <silvioclipper@gmail.com>
2019-12-09 22:38:18 -0300

    Commit ajuste

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-
+var port = process.env.PORT || 3333;
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.listen(3333, ()=>{
-    console.log('Servidor no ar')
+app.listen(port, ()=>{
+    console.log('Servidor no ar ' + port)
 })
